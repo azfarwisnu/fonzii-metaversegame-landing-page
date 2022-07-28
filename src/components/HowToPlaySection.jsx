a51a91114571fc5580de482d3df9ab9071aac554
falbas <farhanalbas30@gmail.com>
2022-07-28 21:42:53 +0700

    update guide page

--- src/components/HowToPlaySection.jsx
+++ src/components/HowToPlaySection.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 AOS.init();
 
@@ -10,15 +10,15 @@ class HowToPlaySection extends React.Component {
     return (
       <>
         <div className="how-to-play-section container mt-4 pt-4 pb-4 mb-4">
-          <div className="top-section row bg-dark p-5 mx-4 mb-4 pb-4">
-            <div className="col-12 col-md-3 left">
+          <div className="top-section row bg-dark p-0 p-md-5 mx-4 mb-4">
+            <div className="col-12 col-md-5 col-lg-3 left">
               <img
                 src="https://www.sandbox.game/img/13_Create/GameMaker/SwordCharacter.png"
                 alt="nft-testing"
                 className="mb-4 img-fluid"
               />
             </div>
-            <div className="col-12 col-md-9 right">
+            <div className="col-12 col-md-12 col-lg-9 right">
               <div className="content">
                 <h3>
                   <b>System requitmens</b>
@@ -26,7 +26,7 @@ class HowToPlaySection extends React.Component {
                 <div className="box-message">
                   <div className="row pt-4">
                     <div className="col-12 col-md-6 mb-4">
-                      <h4>Minimum</h4>
+                      <h4 className="fw-bold">Minimum</h4>
                       <span>
                         Requires 64 bit processor and operating System
                       </span>
@@ -44,7 +44,7 @@ class HowToPlaySection extends React.Component {
                       <span>Storage: 2GB avaible space</span>
                     </div>
                     <div className="col-12 col-md-6 mb-4">
-                      <h4>Recommended</h4>
+                      <h4 className="fw-bold">Recommended</h4>
                       <span>
                         Requires 64 bit processor and operating System
                       </span>
@@ -80,22 +80,22 @@ class HowToPlaySection extends React.Component {
               data-aos-mirror="true"
               data-aos-once="false"
             >
-              <div className="mx-4 mb-4 pb-4">
-                <h3 className="mt-4 pt-4 text-center">
-                  Create Meta Mask Wallet
-                </h3>
-                <iframe
-                  width="100%"
-                  height="350px"
-                  src="https://www.youtube.com/embed/YVgfHZMFFFQ"
-                  title="Metamask Tutorial For COMPLETE Beginners"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                  className="mt-4 rounded text-center px-4"
-                ></iframe>
+              <div className="mx-3 mx-md-4 mb-4 pb-4">
+                <div className="pb-4 border-bottom">
+                  <h3 className="mt-4 pt-3 pt-md-5 text-center fw-bold">
+                    Create Meta Mask Wallet
+                  </h3>
+                  <iframe
+                    src="https://www.youtube.com/embed/YVgfHZMFFFQ"
+                    title="Metamask Tutorial For COMPLETE Beginners"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                    className="mt-4 rounded text-center px-0 px-md-4"
+                  ></iframe>
+                </div>
                 <div
-                  className="bg-dark top-section"
+                  className="bg-dark px-0 px-md-4 border-bottom border-light"
                   data-aos="fade-up"
                   data-aos-offset="50"
                   data-aos-delay="1"
@@ -104,29 +104,27 @@ class HowToPlaySection extends React.Component {
                   data-aos-mirror="true"
                   data-aos-once="false"
                 >
-                  <h3 className="mt-4 pt-4 text-center">
+                  <h3 className="mt-4 pt-5 text-center fw-bold">
                     Connect your MetaMask to The Sandbox + Create an account
                   </h3>
                   <div className="row mt-4">
-                    <div className="col-12 col-md-6 mb-4">
+                    <div className="col-12 mb-4 text-center">
                       <img
                         src="https://miro.medium.com/max/685/0*ljYXe01mhjIYTu6V"
                         alt="guide"
-                        width="100%"
-                        height="100%"
-                        className="rounded"
+                        className="rounded img-fluid"
                       />
                     </div>
-                    <div className="Col-21 col-md-6 mb-4">
+                    <div className="col-12 mb-4">
                       <p>
-                        1. Once you have MetaMask set up,{" "}
+                        1. Once you have MetaMask set up,{' '}
                         <a href="https://www.sandbox.game/en/">click here</a> to
-                        go to The Sandbox’s official website (creator Tomahawk’s
+                        go to The Sandbox's official website (creator Tomahawk's
                         referral link)
                       </p>
                       <p>
                         2. Click the Web 3 (MetaMask) option, shown here (if it
-                        doesn’t appear, click the Sign-In button on the
+                        doesn't appear, click the Sign-In button on the
                         top-right)
                       </p>
                       <p>
@@ -135,39 +133,38 @@ class HowToPlaySection extends React.Component {
                         will allow MetaMask to log you into The Sandbox. Click
                         the Sign button on this prompt.
                       </p>
-                      <img
-                        src="https://miro.medium.com/max/875/0*bB2QtISEIuADyZ1t"
-                        alt="wallet"
-                        className="rounded"
-                        width="100%"
-                      />
+                      <div className="text-center">
+                        <img
+                          src="https://miro.medium.com/max/875/0*bB2QtISEIuADyZ1t"
+                          alt="wallet"
+                          className="rounded img-fluid"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div
-                  className="bg-dark top-section"
+                  className="bg-dark px-0 px-md-4 border-bottom"
                   data-aos="fade-up"
-              data-aos-offset="50"
-              data-aos-delay="1"
-              data-aos-duration="1000"
-              data-aos-easing="ease-in-out"
-              data-aos-mirror="true"
-              data-aos-once="false"
+                  data-aos-offset="50"
+                  data-aos-delay="1"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="false"
                 >
-                  <h3 className="mt-4 pt-4 text-center">
+                  <h3 className="mt-4 pt-5 text-center fw-bold">
                     Create Avatar, Download and Access Sandbox Game Maker
                   </h3>
                   <div className="row mt-4">
-                    <div className="col-12 col-md-6 mb-4">
+                    <div className="col-12 mb-4 text-center">
                       <img
                         src="https://miro.medium.com/max/875/0*iZwI7EHsbEKC1qnp"
                         alt="guide"
-                        width="100%"
-                        height="100%"
-                        className="rounded"
+                        className="rounded img-fluid"
                       />
                     </div>
-                    <div className="Col-21 col-md-6 mb-4">
+                    <div className="col-12 mb-4">
                       <p>
                         1. Select “Avatar” from your profile menu (top right)
                       </p>
@@ -179,17 +176,18 @@ class HowToPlaySection extends React.Component {
                         3. Select the “CREATE” button (left banner) + Game Maker
                         “Download” button.
                       </p>
-                      <img
-                        src="https://miro.medium.com/max/875/0*DUqbeoi1JPIqFwZg"
-                        alt="guide"
-                        className="rounded"
-                        width="100%"
-                      />
+                      <div className="text-center">
+                        <img
+                          src="https://miro.medium.com/max/875/0*DUqbeoi1JPIqFwZg"
+                          alt="guide"
+                          className="rounded img-fluid"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div
-                  className="bg-dark top-section"
+                  className="bg-dark px-0 px-md-4 border-bottom"
                   data-aos="fade-up"
                   data-aos-offset="50"
                   data-aos-delay="1"
@@ -198,20 +196,18 @@ class HowToPlaySection extends React.Component {
                   data-aos-mirror="true"
                   data-aos-once="false"
                 >
-                  <h3 className="mt-4 pt-4 text-center">
+                  <h3 className="mt-4 pt-5 text-center fw-bold">
                     Login, Find and Play "GOG HQ"
                   </h3>
                   <div className="row mt-4">
-                    <div className="col-12 col-md-6 mb-4">
+                    <div className="col-12 mb-4 text-center">
                       <img
                         src="https://miro.medium.com/max/875/0*K_JABOLCxZ19cri3"
                         alt="guide"
-                        width="100%"
-                        height="100%"
-                        className="rounded"
+                        className="rounded img-fluid"
                       />
                     </div>
-                    <div className="Col-21 col-md-6 mb-4">
+                    <div className="col-12 mb-4">
                       <p>
                         1. Access the GameMaker by clicking the shortcut created
                         on your desktop
@@ -224,17 +220,18 @@ class HowToPlaySection extends React.Component {
                         username and password, select the “Draft Gallery” tab —
                         top right
                       </p>
-                      <img
-                        src="https://miro.medium.com/max/875/0*MwedHhkyBqj4c0-b"
-                        alt="guide"
-                        className="rounded"
-                        width="100%"
-                      />
+                      <div className="text-center">
+                        <img
+                          src="https://miro.medium.com/max/875/0*MwedHhkyBqj4c0-b"
+                          alt="guide"
+                          className="rounded img-fluid"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div
-                  className="bg-dark top-section"
+                  className="bg-dark mx-0 mx-md-4"
                   data-aos="fade-up"
                   data-aos-offset="50"
                   data-aos-delay="1"
@@ -243,10 +240,10 @@ class HowToPlaySection extends React.Component {
                   data-aos-mirror="true"
                   data-aos-once="false"
                 >
-                  <h3 className="mt-4 pt-4 text-center">
+                  <h3 className="mt-4 pt-5 text-center fw-bold">
                     Enjoy the game and get reward
                   </h3>
-                  <div className="mx-4">
+                  <div className="mt-4">
                     <img
                       src="https://miro.medium.com/max/875/0*bg7zaM5pqispM6G1"
                       alt="play"
@@ -254,7 +251,7 @@ class HowToPlaySection extends React.Component {
                       width="100%"
                     />
                   </div>
-                  <div className="mx-4 mt-4">
+                  <div className="mt-4">
                     <img
                       src="https://miro.medium.com/max/805/0*jn1KmigueVfRPIpk"
                       alt="play"
@@ -262,12 +259,13 @@ class HowToPlaySection extends React.Component {
                       width="100%"
                     />
                   </div>
-                  <div className="mt-4 pb-4 text-center pt-4">
+                  <div className="mt-4 pb-0 pb-md-4 text-center pt-4">
                     <span>Full Guide</span>
                     <p className="text-white">
                       <a
                         className="text-muted"
                         href="https://guildofguardians.medium.com/gog-x-sandbox-faction-challenge-4233fdfaa80f"
+                        target="blank"
                       >
                         Click here
                       </a>
